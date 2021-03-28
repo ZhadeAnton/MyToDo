d5c6dc84bf29e573c61cc755df693697517ac280
ElGutiar <el12guitar@gmail.com>
2021-03-28 22:29:17 +0300

    added Block, Device and Feedback sections

--- src/pages/main/main.page.tsx
+++ src/pages/main/main.page.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
-import HeroSection from '../../sections/hero-section/hero.component'
+
 import './main.styles.scss'
+import HeroSection from '../../sections/hero-section/SectionHero.component'
+import BlockSection from '../../sections/block-section/SectionBlock.component'
+import
+DeviceSection from '../../sections/device-section/SectionDevice.component'
+import
+FeedbackSection from '../../sections/feedback-section/SectionFeedback.component'
 
 const MainPage: React.FC = () => {
   return (
-    <HeroSection />
+    <>
+      <HeroSection />
+      <BlockSection />
+      <DeviceSection />
+      <FeedbackSection />
+    </>
   )
 }
 
