9e261e3728abb4de110fa05b879cd50a7fa4b210
ElGutiar <el12guitar@gmail.com>
2021-08-04 10:29:43 +0300

    Notifications styles fix

--- src/components/custom/BrandLogo/BrandLogo.tsx
+++ src/components/custom/BrandLogo/BrandLogo.tsx
@@ -1,21 +1,18 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
 
 import styles from './brandLogo.module.scss'
 import { ReactComponent as Logo } from '../../../assets/SVG/checked.svg'
 
 export default function BrandLogo() {
   return (
-    <NavLink to='/'>
-      <div className={styles.brandLogo}>
-        <div className={styles.logoWrapper}>
-          <Logo />
-        </div>
-
-        <p className={styles.title}>
-          My Todo
-        </p>
+    <div className={styles.brandLogo}>
+      <div className={styles.logoWrapper}>
+        <Logo />
       </div>
-    </NavLink>
+
+      <p className={styles.title}>
+        My Todo
+      </p>
+    </div>
   )
 }
